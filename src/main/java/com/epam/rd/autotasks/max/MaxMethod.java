43a16cab415943582d396c4d980a8ecf529f8844
Taghiyev <ktaghiyev01@gmail.com>
2022-11-29 01:39:59 +0100

    max-value

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -2,6 +2,18 @@ package com.epam.rd.autotasks.max;
 
 public class MaxMethod {
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        int maxElem = values[0];
+
+        for (int value : values) {
+            if (maxElem < value) maxElem = value;
+        }
+        return maxElem;
+    }
+
+    public static void main(String[] args) {
+        int[] vals = new int[]{ -2, 0, 10, 5 };
+        int result = MaxMethod.max(vals);
+        System.out.println(result == 10); // true
     }
 }
